931d971f99f97b7f609226b830e849e44a6d1e81
Yusuke Inoue <inouey1008@gmail.com>
2025-09-25 00:38:26 +0900

    Project, Task, TaskStatus テーブル作成

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,4 +1,4 @@
-// import { db } from 'api/src/lib/db'
+import { db } from 'api/src/lib/db'
 
 // Manually apply seeds via the `yarn rw prisma db seed` command.
 //
@@ -9,18 +9,76 @@
 
 export default async () => {
   try {
-    // Create your database records here! For example, seed some users:
-    //
-    // const users = [
-    //   { name: 'Alice', email: 'alice@redwoodjs.com' },
-    //   { name: 'Bob', email: 'bob@redwoodjs.com' },
-    // ]
-    //
-    // await db.user.createMany({ data: users })
+    // Clear project data
+    const project = await db.project.create({
+      data: {
+        title: 'サンプルプロジェクト',
+        detail: 'サンプルプロジェクト詳細',
+      },
+    })
 
-    console.info(
-      '\n  No seed data, skipping. See scripts/seed.ts to start seeding your database!\n'
-    )
+    // Create task status data
+    const todoStatus = await db.taskStatus.create({
+      data: {
+        title: 'ToDo',
+        detail: '未着手',
+        projectId: project.id,
+      },
+    })
+
+    const doingStatus = await db.taskStatus.create({
+      data: {
+        title: 'Doing',
+        detail: '進行中',
+        projectId: project.id,
+      },
+    })
+
+    const doneStatus = await db.taskStatus.create({
+      data: {
+        title: 'Done',
+        detail: '完了',
+        projectId: project.id,
+      },
+    })
+
+    // Create task
+    const tasks = [
+      {
+        title: 'Backlogタスク1',
+        detail: 'Backlogタスク1の詳細',
+        projectId: project.id,
+        statusId: null, // バックログ
+      },
+      {
+        title: 'Backlogタスク2',
+        detail: 'Backlogタスク2の詳細',
+        projectId: project.id,
+        statusId: null, // バックログ
+      },
+      {
+        title: 'ToDoタスク',
+        detail: 'ToDoタスクの詳細',
+        projectId: project.id,
+        statusId: todoStatus.id,
+      },
+      {
+        title: 'Doingタスク',
+        detail: 'Doingタスクの詳細',
+        projectId: project.id,
+        statusId: doingStatus.id,
+      },
+      {
+        title: 'Doneタスク',
+        detail: 'Doneタスクの詳細',
+        projectId: project.id,
+        statusId: doneStatus.id,
+      },
+    ]
+
+    await db.task.createMany({ data: tasks })
+
+    console.info('DB seeded successfully')
   } catch (error) {
     console.error(error)
   }
